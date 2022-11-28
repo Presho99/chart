2a6a9755098737b8fa1ba1c2ce8bb22e752532e1
Precious <Preshomondi@gmail.com>
2022-11-28 17:03:04 +0300

    create and style Main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Sidebar from './components/Sidebar';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <div className='App-inner'>
         <Sidebar/>
+        <Main/>
       </div>
     </div>
   );
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../components/Main.css"
+import Cards from './Cards'
+
+function Main() {
+  return (
+    <div className='main'>
+        main
+        <Cards/>
+    </div>
+  )
+}
+
+export default Main
